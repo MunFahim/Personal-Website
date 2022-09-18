4db4579612ff8b120f54f16ec358a145a1391655
MunFahim <muntasirf362@gmail.com>
2022-09-17 20:02:23 -0400

    removed unused code

--- school.js
+++ school.js
@@ -5,23 +5,18 @@ function works(){
 var turn = true;
 
 function rotateButton(){
-    //var button = document.querySelector("#icon");
-    //var button = getElementById("icon");
-    //button.className = button.className ? "" : "dropButton";
-    //button.style.rotate="90deg";
+   
 
 
     var x = document.getElementById("icon");
     var p = document.getElementById("dropDownContent");
     if (turn) {
     x.style.rotate = "90deg";
-    //y.style.visibility = "visible";
-    //k.style.visibility = "visible";
+    
     p.style.visibility = "visible";
     } else {
     x.style.rotate = "0deg";
-    //y.style.visibility = "hidden";
-    //k.style.visibility = "hidden";
+    
     p.style.visibility = "hidden";
     } 
     turn = !turn;
